1cfc316d62381b7d4c8f7addd7502365b2cd6f73
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-10-22 19:40:00 -0300

    3.8.c

--- 3.8.c
+++ 3.8.c
@@ -1,31 +1,54 @@
+/* 
+Escrever um programa que examina um numero e diz quantos 33 tem dentro dele.
+*/
 #include <stdio.h>
 
 int main() {
 
-  int n;
+  int n; //Usuario introduz um numero
 
   printf("Entre com n: ");
   scanf("%d", &n );
 
-  int tinha33 = 0; /*INICIALIZAÇÃO Uma variável chamada tinha33 é declarada e inicializada com 0. 
-  Esta variável será usada para indicar se a sequência "33" foi encontrada no número.*/
+  int tinha33 = 0; //contador inicia em 0
 
-  while ( n > 0 ) { // CONDIÇÃO Entra em um loop while que continua enquanto n for maior que zero.
-    if ( n % 100 == 33 ) {/*Verifica se os dois ultimos digitos de n são iguais
-    a 22 usando a operação n%100 == 33, pega os dois ultimos digitos de n, 
-    se essa condição for verdadeira significa qur a sequencia 33 foi encontrada*/
-      tinha33 = 1; //Tinha 33 =1 loop interrompido.
-      break;
+  while ( n > 0 ) { //N tem que ser maior que 0
+    if ( n % 100 == 33 ) { //Se o resto da divisao de n por 100 for igual a 33 roda o seguinte:
+      tinha33 = 1; //Tem 33 e da um  break
+      break; //Programa encerra
     }
-    n = n / 10; /*A linha de código n = n / 10; é usada para remover
-     o último dígito do número inteiro n. Isso é feito 
-     dividindo n por 10, 
-    o que resulta na remoção do dígito da direita.*/
+    n = n / 10; //Se nao o valor n é dividido por 10 ate tentar achar um n com 33.
   }
 
-  if ( tinha33 == 1 ) //se tinha33 = 1 tem 33 se nao, nao tem
+  if ( tinha33 == 1 )
     printf("Tem um 33\n");
   else
     printf("Nao tem um 33\n");
 
 }
+
+/*
+Esse programa em C tem o objetivo de verificar se um número n fornecido pelo usuário contém o dígito "33" em qualquer parte de sua representação decimal. Vou explicar o programa passo a passo:
+
+Declaração de variáveis:
+
+O programa começa declarando as variáveis necessárias:
+int n: Esta variável armazenará o número fornecido pelo usuário.
+int tinha33 = 0: Esta variável é uma flag que é inicializada com 0. Ela será usada para indicar se o número contém o dígito "33".
+Entrada de dados:
+
+O programa solicita que o usuário insira um número inteiro n e armazena o valor inserido na variável n.
+Loop while:
+
+O programa entra em um loop while que continuará até que n seja maior que zero.
+No interior do loop:
+Verifica se os dois últimos dígitos de n são iguais a "33" usando a condição if (n % 100 == 33).
+Se essa condição for verdadeira, o programa define a variável tinha33 como 1 (indicando que encontrou "33") e sai do loop usando break.
+Caso contrário, o programa remove o último dígito de n dividindo-o por 10 (n = n / 10) para continuar verificando os dígitos restantes.
+Verificação e saída:
+
+Após o loop, o programa verifica o valor de tinha33.
+Se tinha33 for igual a 1, o programa imprime "Tem um 33" para indicar que o número contém o dígito "33".
+Caso contrário, se tinha33 for igual a 0, o programa imprime "Nao tem um 33" para indicar que o número não contém o dígito "33".
+Resumindo, o programa verifica os dois últimos dígitos do número n em cada iteração do loop while. Se encontrar "33" em algum lugar do número, a flag tinha33 é definida como 1, e o programa imprime que "Tem um 33". Caso contrário, se o loop terminar sem encontrar "33", o programa imprime "Nao tem um 33".
+*/

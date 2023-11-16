5fe314ddf9201a1bbca2933b2d016fc819b15714
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-11-16 13:52:41 -0300

    2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,29 +1,20 @@
-#include<stdio.h.>
-
+//Dois produtos tem um desconto de 50% no produto mais barato, escreva aonde recebe dois preços e calcula o valor final pago.
+#include <stdio.h>
 int main(){
+  int p1,p2;
 
-int p1, p2;
-int p1_maisbarato, p2_maisbarato;
-int valortotal2, valortotal1;
+  printf("Preço dos dois produtos para aplicar o desconto:\n");
+  scanf("%d %d", &p1, &p2);
 
-printf("Insira o valor dos seus produtos:\n");
-scanf("%d %d", &p1, &p2);
-    
-    if(p1>p2){
-        p2_maisbarato = p2 *0.5;
-    printf("O Produto mais barato custava %d, mas com o desconto agora tera um custo de %d!\n", p2, p2_maisbarato);
-        valortotal2 = p1 + p2_maisbarato; 
-    printf("***O valor final da sua compra sera: %d***", valortotal2);
-    }
-
-    else{
-        p1_maisbarato = p1 * 0.5;
-    printf("O Produto mais barato custava %d, mas com o desconto agora tera um custo de %d!\n", p1, p1_maisbarato);
-        valortotal1 = p2 + p1_maisbarato; 
-    printf("O valor final da sua compra sera: %d", valortotal2);
-
-    }
-
-    
-    return 0;
+  if(p1>p2){
+    int desc2 = p2*0.5;
+    int vf2 = desc2 + p1;
+    printf("Valor normal:R$%d\n Peça com desconto:R$:%d\nValorTotal:R$%d", p1,desc2, vf2);
+  }
+  if(p1<p2){
+    int desc1 = p1*0.5;
+    int vf1 = desc1 + p2;
+    printf("Valor normal:R$%d\n Peça com desconto:R$:%d\nValorTotal:R$%d", p2,desc1, vf1);
+  }
+  return 0;
 }

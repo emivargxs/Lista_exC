2a75d2951e551bd2b78d14889d7232c4c470bbf1
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-11-30 20:19:12 -0300

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,20 +1,20 @@
-//Dois produtos tem um desconto de 50% no produto mais barato, escreva aonde recebe dois preços e calcula o valor final pago.
-#include <stdio.h>
+//Dois produtos, 50% no mais barato
+
+#include<stdio.h>
 int main(){
   int p1,p2;
-
-  printf("Preço dos dois produtos para aplicar o desconto:\n");
+  printf("Valor do Produto 1 e Produto 2:\n"); 
   scanf("%d %d", &p1, &p2);
 
   if(p1>p2){
-    int desc2 = p2*0.5;
-    int vf2 = desc2 + p1;
-    printf("Valor normal:R$%d\n Peça com desconto:R$:%d\nValorTotal:R$%d", p1,desc2, vf2);
+    int p2barato = p2 * 0.5;
+    printf("%d valor final, %d sem desconto", p2barato, p2);
   }
-  if(p1<p2){
-    int desc1 = p1*0.5;
-    int vf1 = desc1 + p2;
-    printf("Valor normal:R$%d\n Peça com desconto:R$:%d\nValorTotal:R$%d", p2,desc1, vf1);
+  else{
+    int p1barato = p1 * 0.5;
+    printf("%d valor final, %d sem desconto", p1barato, p1);
   }
+
+  
   return 0;
 }
